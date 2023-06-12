79bf950a561cac0280cb72eca0de996813159f2c
이호준 <hjlee@kai-i.com>
2023-06-12 13:02:25 +0900

    백준 10816 > 숫자 카드 2

---
+++ src/baekjoon/Baekjoon_10816.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String num = st.nextToken();
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            sb.append(map.getOrDefault(st.nextToken(), 0)).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
