09111b017600052878da582a9cd0d44cde5382ed
이호준 <psh10066@gmail.com>
2024-02-05 22:14:11 +0900

    백준 2003 > 수들의 합 2 (투 포인터)

---
+++ src/baekjoon/Baekjoon_2003.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_2003 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = 0;
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            if (sum >= m) {
+                sum -= arr[start++];
+            } else if (end == n) {
+                break;
+            } else {
+                sum += arr[end++];
+            }
+            if (sum == m) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
