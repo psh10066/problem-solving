4216c5b7f642eff76c0684a06618c62568d7949d
이호준 <psh10066@gmail.com>
2025-11-03 22:46:03 +0900

    Codility > MaxProductOfThree

---
+++ src/codility/Codility_MaxProductOfThree.java
@@ -0,0 +1,14 @@
+package codility;
+
+import java.util.Arrays;
+
+public class Codility_MaxProductOfThree {
+
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        return Math.max(
+            A[0] * A[1] * A[A.length - 1],
+            A[A.length - 1] * A[A.length - 2] * A[A.length - 3]
+        );
+    }
+}
